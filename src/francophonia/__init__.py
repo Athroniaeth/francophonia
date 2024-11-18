130c1ec0ed219a76777cf0d5fc222d3cf548e450
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-11-18 11:01:05 +0100

    Update, refactor build path emplacement

--- src/francophonia/__init__.py
+++ src/francophonia/__init__.py
@@ -6,5 +6,8 @@ PROJECT_PATH = Path(__file__).resolve().parents[2]
 SRC_PATH = PROJECT_PATH / 'src'
 APP_PATH = SRC_PATH / 'francophonia'
 
+DOCS_PATH = PROJECT_PATH / 'docs'
+BUILD_PATH = PROJECT_PATH / 'site'
+
 # Set the logger for the project
 logger = logging.getLogger("francophonia")
